122efc086ec84a914be07430851891b18a4e98be
Ubuntu <ubuntu@ip-172-31-45-124.ec2.internal>
2024-07-13 00:35:13 +0000

    Mudança do domínio

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -8,11 +8,12 @@ import java.net.*;
  */
 public class App 
 {
+    
     public static void main( String[] args )
     {
         try{
             
-            String name = "google.com";
+            String name = "github.com";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
             System.out.println( "Address:   "+ address.getHostAddress());
